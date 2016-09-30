4ebdc9871e14394214879e4e40ba36c3dba875ec
Alessandro Desantis <desa.alessandro@gmail.com>
2016-09-30 14:31:07 +0200

    Implement #normalize_all in AvailabilityCollection

--- lib/artic/collection/availability_collection.rb
+++ lib/artic/collection/availability_collection.rb
@@ -2,24 +2,40 @@
 module Artic
   module Collection
     class AvailabilityCollection < Array
-      def normalize
-        normalized_availabilities = sort.group_by(&:identifier).values.flat_map do |availabilities|
-          availabilities.inject([]) do |accumulator, availability|
-            next (accumulator << availability) if accumulator.empty?
+      def by_identifier(identifier)
+        select { |availability| availability.identifier == identifier }
+      end
 
-            last_availability = accumulator.pop
+      def identifiers
+        map { |availability| availability.identifier }.uniq
+      end
 
-            next (
-              accumulator + [last_availability, availability]
-            ) unless last_availability.time_range.overlaps?(availability.time_range)
+      def normalize_all
+        normalized_availabilities = identifiers.flat_map do |identifier|
+          normalize identifier
+        end
 
-            new_time_range = Range.new(
-              [last_availability.time_range.min, availability.time_range.min].min,
-              [last_availability.time_range.max, availability.time_range.max].max
-            )
+        self.class.new normalized_availabilities.sort
+      end
 
-            accumulator << Availability.new(availability.identifier, new_time_range)
-          end
+      def normalize(identifier)
+        availabilities = by_identifier(identifier).sort
+
+        normalized_availabilities = availabilities.inject([]) do |accumulator, availability|
+          next (accumulator << availability) if accumulator.empty?
+
+          last_availability = accumulator.pop
+
+          next (
+            accumulator + [last_availability, availability]
+          ) unless last_availability.time_range.overlaps?(availability.time_range)
+
+          new_time_range = Range.new(
+            [last_availability.time_range.min, availability.time_range.min].min,
+            [last_availability.time_range.max, availability.time_range.max].max
+          )
+
+          accumulator << Availability.new(availability.identifier, new_time_range)
         end
 
         self.class.new normalized_availabilities
--- spec/artic/collection/availability_collection_spec.rb
+++ spec/artic/collection/availability_collection_spec.rb
@@ -13,13 +13,45 @@ RSpec.describe Artic::Collection::AvailabilityCollection do
     ]
   end
 
+  describe '#identifiers' do
+    it 'returns all identifiers in the collection' do
+      expect(collection.identifiers).to match_array([
+        Date.parse('2016-09-30'),
+        :monday,
+        :tuesday
+      ])
+    end
+  end
+
+  describe '#by_identifier' do
+    it 'returns all availabilities for the given identifier' do
+      expect(collection.by_identifier(Date.parse('2016-09-30'))).to match_array([
+        Artic::Availability.new(Date.parse('2016-09-30'), '09:00'..'17:00'),
+        Artic::Availability.new(Date.parse('2016-09-30'), '12:00'..'18:00')
+      ])
+    end
+  end
+
   describe '#normalize' do
     it 'returns another AvailabilityCollection' do
-      expect(collection.normalize).to be_instance_of(described_class)
+      expect(collection.normalize(:monday)).to be_instance_of(described_class)
     end
 
-    it 'sorts and merges contiguous slots together' do
-      expect(collection.normalize).to match_array([
+    it 'sorts and merges slots for that identifier' do
+      expect(collection.normalize(:monday)).to eq([
+        Artic::Availability.new(:monday, '09:00'..'13:00'),
+        Artic::Availability.new(:monday, '15:00'..'19:00')
+      ])
+    end
+  end
+
+  describe '#normalize_all' do
+    it 'returns another AvailabilityCollection' do
+      expect(collection.normalize_all).to be_instance_of(described_class)
+    end
+
+    it 'sorts and merges all slots' do
+      expect(collection.normalize_all).to eq([
         Artic::Availability.new(:monday, '09:00'..'13:00'),
         Artic::Availability.new(:monday, '15:00'..'19:00'),
         Artic::Availability.new(:tuesday, '09:00'..'18:00'),
